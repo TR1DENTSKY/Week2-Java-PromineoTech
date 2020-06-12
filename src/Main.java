bae146ee2c6e27b6819732df0bbbfc658878ed62
Jonathan Mai <23644860+TR1DENTSKY@users.noreply.github.com>
2020-06-12 13:15:52 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,16 @@ public class Main {
         int thirstLevel = 3;
 
         boolean shouldByIcecream = isHotOutside && hasMoneyInPocket;
+        System.out.println("Its hot outside and there is money in my pocket = " + shouldByIcecream);
+
         boolean willGoSwimming = isHotOutside && !isWeekday;
+        System.out.println("Its hot outside and it is not a weekday = " + willGoSwimming);
+
         boolean isAGoodDay = isHotOutside && hasMoneyInPocket && !isWeekday;
+        System.out.println("Its hot outside, there's money in my pocket, and its not a weekday = " + isAGoodDay);
+
         boolean willBuyMilk = isHotOutside && (thirstLevel >= 3) && (moneyInWallet >= costOfMilk * 2);
+        System.out.println("Its hot outside, thirstLevel is >= to 3, money is >= 2x cost of milk = " + willBuyMilk);
 
 
     }
